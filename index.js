0a2aae2ade1d61df423cf93b3e1c8fe7472ff6cc
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-02-13 19:41:44 -0300

    improve verification and show result

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function generateNumber () {
     const inputMin = Math.ceil(document.getElementById("input-min").value)
     const inputMax = Math.floor(document.getElementById("input-max").value)
-    const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin)
 
-    console.log(`O número sorteado é: ${result}`)
-    alert(`O número sorteado é: ${result}`)
+    if(inputMax <= inputMin) {
+        alert(`O segundo número precisa ser maior que o primeiro!`)
+    } else {
+        const result = Math.floor(Math.random() * (inputMax - inputMin) + inputMin)
+        const titleResult = document.querySelector(".titleResult")
+        const valueResult = document.querySelector(".valueResult")
+
+        titleResult.classList.remove("opacity")
+        valueResult.classList.remove("opacity")
+        valueResult.innerText = result
+
+        console.log(`O número sorteado é: ${result}`)
+    }
 }
